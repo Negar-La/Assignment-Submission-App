3aa1aa63689935ceb0f50acbaa9bd5068b2cd420
Negar Lashgari <negar.lashgari@gmail.com>
2024-01-12 14:05:38 -0500

    leverage a useState hook for Local Storage in ReactJS

--- AssignmentSubmissionApp/front-end/src/App.js
+++ AssignmentSubmissionApp/front-end/src/App.js
@@ -1,32 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
+import { useLocalStrorage } from "./util/useLocalStorage";
 
 function App() {
   // console.log("hi");
-  const [jwt, setJwt] = useState("");
+  // const [jwt, setJwt] = useState("");   //replaced by useLocalStrorage
+  const [jwt, setJwt] = useLocalStrorage("", "jwt");
 
   useEffect(() => {
-    const reqbody = {
-      username: "arash",
-      password: "arash",
-    };
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(reqbody),
-    };
+    if (!jwt) {
+      const reqbody = {
+        username: "arash",
+        password: "arash",
+      };
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(reqbody),
+      };
 
-    fetch("api/auth/login", requestOptions)
-      .then((responce) => Promise.all([responce.json(), responce.headers]))
-      .then(([body, headers]) => {
-        setJwt(headers.get("authorization"));
+      fetch("api/auth/login", requestOptions)
+        .then((responce) => Promise.all([responce.json(), responce.headers]))
+        .then(([body, headers]) => {
+          setJwt(headers.get("authorization"));
 
-        // const jwt = headers.get("authorization"); was replaced by setJwt
-        // console.log(jwt);
-        // console.log(body);
-      });
+          // const jwt = headers.get("authorization"); was replaced by setJwt
+          // console.log(jwt);
+          // console.log(body);
+        });
+    }
   }, []);
 
   useEffect(() => {
