0ab51fbca1deac36650f59d1e98ff3f0ada830b4
Negar Lashgari <negar.lashgari@gmail.com>
2024-01-14 20:20:30 -0500

    Connecting a ReactJS Login Page to a RESTful API

--- AssignmentSubmissionApp/front-end/src/App.js
+++ AssignmentSubmissionApp/front-end/src/App.js
@@ -10,33 +10,7 @@ import PrivateRoute from "./Components/PrivateRoute";
 function App() {
   // console.log("hi");
   // const [jwt, setJwt] = useState("");   //replaced by useLocalStrorage
-  const [jwt, setJwt] = useLocalStrorage("", "jwt");
-
-  // useEffect(() => {
-  //   if (!jwt) {
-  //     const reqbody = {
-  //       username: "arash",
-  //       password: "arash",
-  //     };
-  //     const requestOptions = {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify(reqbody),
-  //     };
-
-  //     fetch("api/auth/login", requestOptions)
-  //       .then((responce) => Promise.all([responce.json(), responce.headers]))
-  //       .then(([body, headers]) => {
-  //         setJwt(headers.get("authorization"));
-
-  //         // const jwt = headers.get("authorization"); was replaced by setJwt
-  //         // console.log(jwt);
-  //         // console.log(body);
-  //       });
-  //   }
-  // }, []);
+  const [jwt] = useLocalStrorage("", "jwt");
 
   useEffect(() => {
     console.log("JWT is " + jwt);
