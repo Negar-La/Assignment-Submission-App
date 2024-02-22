44d018fbbacf532c6fd6ebea37cd665334fab524
Negar Lashgari <negar.lashgari@gmail.com>
2024-02-22 00:59:22 -0500

    Added Assignment Statuses to our App

--- AssignmentSubmissionApp/src/main/java/com/demo/AssignmentSubmission/dto/AssignmentResponseDto.java
+++ AssignmentSubmissionApp/src/main/java/com/demo/AssignmentSubmission/dto/AssignmentResponseDto.java
@@ -2,13 +2,14 @@ package com.demo.AssignmentSubmission.dto;
 
 import com.demo.AssignmentSubmission.domain.Assignment;
 import com.demo.AssignmentSubmission.enams.AssignmentEnum;
+import com.demo.AssignmentSubmission.enams.AssignmentStatusEnum;
 
 public class AssignmentResponseDto {
     //in AssignmentController for    @GetMapping("{id}") we are sending assignmentOpt to frontend
     //But because we wante to add more data to assignment (Enums), we create this DTO that represents this response
     private Assignment assignment;
     private AssignmentEnum[] assignmentEnums = AssignmentEnum.values();
-
+    private AssignmentStatusEnum[] statusEnums = AssignmentStatusEnum.values();
     public AssignmentResponseDto(Assignment assignment) {
         this.assignment = assignment;
     }
@@ -23,5 +24,9 @@ public class AssignmentResponseDto {
 
     public AssignmentEnum[] getAssignmentEnums() {
         return assignmentEnums;
+    }
+
+    public AssignmentStatusEnum[] getStatusEnums() {
+        return statusEnums;
     }
 }
